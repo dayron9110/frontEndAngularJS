6c7bcbd7532b8a8079416fc24e987a085390bd7d
dayron91 <dayroncediel@gmail.com>
2017-10-17 08:14:59 -0500

    initial commit 2

---
+++ app/app.js
@@ -0,0 +1,16 @@
+'use strict';
+
+// Declare app level module which depends on views, and components
+angular.module('myApp', [
+  'ngRoute',
+  'myApp.view1',
+  'myApp.view2',
+  'myApp.version'
+]).
+config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
+  $locationProvider.hashPrefix('!');
+
+  $routeProvider.otherwise({redirectTo: '/view1'});
+}]);
+var app = angular.module('userRecords', [])
+.constant('API_URL', 'http://localhost/backendLaravel/public/api/v1/');
